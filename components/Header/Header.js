ecbb5d00fdde8fca212b67a5c060ad83892b0cd5
IveTou <ive_tourinho@hotmail.com>
2022-05-19 20:45:54 -0300

    feat(style): header with icon and toolbar

---
+++ components/Header/Header.js
@@ -0,0 +1,17 @@
+import { memo } from 'react'
+
+import UIBase from '../../containers/UIBase'
+import S from './Header.style'
+
+const Header = () => (
+  <UIBase>
+    <S.Header>
+      <S.Content>
+        <S.Icon>Calyo</S.Icon>
+        <S.ToolBar></S.ToolBar>
+      </S.Content>
+    </S.Header>
+  </UIBase>
+)
+
+export default memo(Header)
--- containers/Layout/Layout.js
+++ containers/Layout/Layout.js
@@ -1,8 +1,10 @@
 import S, { GlobalStyle } from './Layout.style'
+import Header from '../../components/Header/Header'
 
 const Layout = ({ children }) => (
   <S.Layout>
     <GlobalStyle />
+    <Header />
     <S.Main>
       {children}
     </S.Main>
